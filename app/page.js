d4a6ffa84e5ad4fba0d26d6f364acec903f32de7
TTeuber <100668008+TTeuber@users.noreply.github.com>
2022-12-12 17:18:29 -0800

    clear homepage and make task list

--- app/page.js
+++ app/page.js
@@ -1,31 +1,8 @@
-"use client";
-
-import {useCallback, useEffect, useState} from "react";
-import Image from "next/image";
-
 export default function Home() {
-    const [pic, setPic] = useState([]);
-    const getImage = useCallback((url) => {
-        fetch(url).then((res) => {
-            res.blob().then((data) => {
-                setPic((x) => [...x, URL.createObjectURL(data)])
-            })
-        })
-    }, [])
-    useEffect(() => {
-        fetch("/api/photoNames/Animals").then((res) => {
-            res.json().then((data) => {
-                data.names.forEach((name) => getImage(`/api/${name}`))
-            })
-        })
-        return setPic([]);
-    }, [getImage])
-  return (
-      <div>
-          {pic.map((p, i) => {
-            return <Image src={p} alt={"test"} width={400} height={400} key={i}/>
-          })}
-      </div>
-  )
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
 }
 
---
+++ tasks.md
@@ -0,0 +1,18 @@
+# Tasks
+
+## Home Screen
+- Introduction?
+- some cool ui demo
+- something functional?
+- skills?
+
+## Music
+- Scrubber
+  - background
+  - functionality
+- Song list
+  - name
+  - photo?
+
+## Blogs
+- create blogs?
