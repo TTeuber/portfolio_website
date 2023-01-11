c6da9dd661a4c3b2b23d9ab8ff3b09aac72ad208
TTeuber <100668008+TTeuber@users.noreply.github.com>
2023-01-11 00:08:21 -0800

    prepare for deployment

--- app/blogs/[id]/page.js
+++ app/blogs/[id]/page.js
@@ -1,17 +1,13 @@
 import Link from "next/link";
-import PocketBase from 'pocketbase';
-
-const client = new PocketBase('http://127.0.0.1:8090')
 
 export default async function BlogPost({params, searchParams}) {
-    const result = await client.records.getOne('blogs', params.id);
 
     return (
         <div className={"flex flex-col min-h-screen"}>
             <Link href={"/blogs"} className={"absolute"}>{"<"}Back</Link>
-            <h1 className={"text-center mb-20 text-8xl mt-0"}>{result.title}</h1>
+            <h1 className={"text-center mb-20 text-8xl mt-0"}>Title</h1>
             <div className={"flex justify-center px-6 flex-grow"}>
-                <p className={"w-full p-6 bg-gray-800 rounded-2xl text-2xl border-2 border-gray-700 shadow-2xl"}>{result.text}</p>
+                <iframe src={"/blogs/test.html"} className={"w-full p-6 bg-gray-800 rounded-2xl text-2xl border-2 border-gray-700 shadow-2xl"}></iframe>
             </div>
         </div>
     )
--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,16 +1,16 @@
 import Link from "next/link";
-import PocketBase from 'pocketbase';
 import Image from "next/image";
 
-const client = new PocketBase('http://127.0.0.1:8090')
-
 export default async function Blogs() {
-    const results = await client.records.getFullList('blogs');
+    const items = [
+        {id: "1", title: "Test Title", subtitle: "This is a test subtitle."},
+        {id: "2", title: "Test 2 Title", subtitle: "This is another test subtitle."}
+    ];
 
     return (
             <div className={"text-gray-400 text-6xl lg:text-8xl flex flex-col items-center justify-center"}>
                 <h1 className={"mb-20"}>Blogs Title</h1>
-                {results.map((item) => <BlogItem key={item.id} blog={item}/>)}
+                {items.map((item) => <BlogItem key={item.id} blog={item}/>)}
             </div>
     )
 }
@@ -21,7 +21,7 @@ function BlogItem({blog}) {
             <div className={"border-2 border-gray-700 flex text-6xl mb-4 rounded-2xl bg-gray-800 group-hover:text-gray-300"}>
                 <div className={"flex justify-center items-center fixed -z-10 lg:static lg:z-0"}>
                     <div className={"border border-black rounded-full overflow-hidden w-40 h-40 relative group-hover:w-44 group-hover:h-44 ease-in-out duration-200 m-4"}>
-                        <Image src={"/../public/kaia_lights.jpg"} width={40} height={20} alt={"kaia"} className={"w-auto h-full object-cover relative"}/>
+                        <Image src={"/../public/kaia_lights.jpg"} width={400} height={200} alt={"kaia"} className={"w-auto h-full object-cover relative"}/>
                     </div>
                 </div>
                 <div className={"lg:border-l-2 border-gray-700 py-2 px-4"}>
