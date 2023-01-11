22975fa9237ab2128b1a059771d9493ea01e01ce
TTeuber <100668008+TTeuber@users.noreply.github.com>
2023-01-11 02:24:44 -0800

    fixed photo path

--- app/photos/page.jsx
+++ app/photos/page.jsx
@@ -25,18 +25,18 @@ export default function Photos() {
 }
 
 function Album(props) {
-    const photos = fs.readdirSync(path.resolve(".", imgPath + props.name + '/'));
+    const photos = fs.readdirSync(path.resolve(".", imgPath + props.name));
     return (
         <Link href={`/photos/${props.name}`} className={"flex flex-col items-center"}>
             <div className={"w-80 h-64 translate-x-8 hover:z-10 group hover:cursor-pointer"}>
                 <div className={"mb-8 border-4 border-black h-60 w-60 sm:h-50 sm:w-50 lg:w-60 lg:h-60 xl:h-64 xl:w-64 flex justify-center absolute z-10 group-hover:-translate-y-4 ease-in-out duration-300"}>
-                    <Image src={`/${props.name}/${photos[photos.length - 1]}`} loading={"lazy"} alt={"photo 1"} height={400} width={400} className={"object-top object-cover relative h-full shadow-2xl"}/>
+                    <Image src={`/images/${props.name}/${photos[photos.length - 1]}`} loading={"lazy"} alt={"photo 1"} height={400} width={400} className={"object-top object-cover relative h-full shadow-2xl"}/>
                 </div>
                 <div className={"mb-8 border-4 border-black h-60 w-60 sm:h-50 sm:w-50 lg:w-60 lg:h-60 xl:h-64 xl:w-64 flex justify-center rotate-6 translate-x-8 absolute group-hover:translate-x-12 group-hover:rotate-12 ease-in-out duration-300"}>
-                    <Image src={`/${props.name}/${photos[Math.floor(photos.length/3) * 2]}`} loading={"lazy"} alt={"photo 2"} height={400} width={400} className={"object-top object-cover relative h-full shadow-2xl"}/>
+                    <Image src={`/images/${props.name}/${photos[Math.floor(photos.length/3) * 2]}`} loading={"lazy"} alt={"photo 2"} height={400} width={400} className={"object-top object-cover relative h-full shadow-2xl"}/>
                 </div>
                 <div className={"mb-8 border-4 border-black h-60 w-60 sm:h-50 sm:w-50 lg:w-60 lg:h-60 xl:h-64 xl:w-64 flex justify-center -rotate-6 -translate-x-8 absolute group-hover:-translate-x-12 group-hover:-rotate-12 ease-in-out duration-300"}>
-                    <Image src={`/${props.name}/${photos[Math.floor(photos.length/3)]}`} loading={"lazy"} alt={"photo 3"} height={400} width={400} className={"object-top object-cover relative h-full shadow-2xl"}/>
+                    <Image src={`/images/${props.name}/${photos[Math.floor(photos.length/3)]}`} loading={"lazy"} alt={"photo 3"} height={400} width={400} className={"object-top object-cover relative h-full shadow-2xl"}/>
                 </div>
             </div>
             <h1 className={"text-4xl my-4"}>{props.name}</h1>
