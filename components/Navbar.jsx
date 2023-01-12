bfea64eb26750e34df6b4c3c9832ebbc24131fbd
TTeuber <100668008+TTeuber@users.noreply.github.com>
2023-01-11 19:50:40 -0800

    fixed navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     return (
         <div className={"flex flex-row text-gray-400 p-6 text-4xl fixed inset-0 top-0 w-screen z-50 justify-center h-fit"}>
             <div className={"fixed inset-0 bg-gray-700 h-24 w-screen bg-opacity-90 z-40 backdrop-blur-md"}></div>
-            <div className={"w-full lg:w-3/4 gap-4 flex flex-row gap-4 z-50 h-fit fixed sm:static z-0 sm:z-40"}>
+            <div className={"w-full lg:w-3/4 gap-4 flex flex-row gap-4 -z-50 h-fit fixed sm:static z-0 sm:z-40"}>
                 <Link href={"/"} className={"hover:text-gray-300"}>Home</Link>
                 <Link href={"/photos"} className={"hover:text-gray-300"}>Photos</Link>
                 <Link href={"/music"} className={"hover:text-gray-300"}>Music</Link>
